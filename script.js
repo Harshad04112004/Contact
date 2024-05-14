1f6a4e37c6c7e2d907ba5753935c6b603ad1fe16
Harshad04112004 <144579615+Harshad04112004@users.noreply.github.com>
2024-05-14 19:03:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sumbmit(){
+    var a= document.getElementsByClassName('Contact-inputs').value;
+    if (a!=" "){
+        alert("data is not save in any location")
+    } else {
+    alert("you data save in host location")
+}
+}
